633c34c69af075237b1ff68679fe3faf6c483bd3
Clyzer <zenit4d@gmail.com>
2022-09-17 10:54:01 -0300

    Archivos reorganizados

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hola mundo
+      </header>
+    </div>
+  );
+}
+
+export default App;
